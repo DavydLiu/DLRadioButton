3ffaf0442822803cb8ff25bd7b16b5f843f27260
David <xingruo.liu@gmail.com>
2015-07-11 09:29:26 -0700

    Add color support for selected state

--- DLRadioButton/DLRadioButton.h
+++ DLRadioButton/DLRadioButton.h
@@ -1,64 +1,74 @@
 #import <UIKit/UIKit.h>
 
 /**
- A hightly customizable Radio Button for iOS
+ A hightly customizable Radio Button for iOS.
 */
 IB_DESIGNABLE
 @interface DLRadioButton : UIButton
 
 /**@name Properties*/
 /**
- Container for holding other buttons
+ Container for holding other buttons in same group.
 */
 @property (nonatomic) IBOutletCollection(DLRadioButton) NSArray *otherButtons;
+
 /**
- Image for radio button icon (optional)
-*/
-@property (nonatomic) IBInspectable UIImage *icon;
-/**
- Image for radio button icon when selected (optional)
-*/
-@property (nonatomic) IBInspectable UIImage *iconSelected;
-/**
- Size of icon
+ Size of icon, default is kDefaulIconSize.
 */
 @property (nonatomic) IBInspectable CGFloat iconSize;
+
 /**
- Color of icon
+ Color of icon, default is title color for current UIControlState.
  */
 @property (nonatomic) IBInspectable UIColor *iconColor;
+
 /**
- Stroke width of icon
+ Stroke width of icon, default is iconSize / 9.
  */
 @property (nonatomic) IBInspectable CGFloat iconStrokeWidth;
+
 /**
- Size of selection indicator
+ Size of selection indicator, default is iconSize * 0.5.
  */
 @property (nonatomic) IBInspectable CGFloat indicatorSize;
+
 /**
- Color of selection indicator
+ Color of selection indicator, default is title color for current UIControlState.
 */
 @property (nonatomic) IBInspectable UIColor *indicatorColor;
+
 /**
- Margin width between icon and title
+ Margin width between icon and title, default is kDefaultMarginWidth.
  */
 @property (nonatomic) IBInspectable CGFloat marginWidth;
+
 /**
- Whether icon on the right side
+ Whether icon on the right side.
 */
 @property (nonatomic) IBInspectable BOOL isIconOnRight;
+
 /**
- Whether use square icon
+ Whether use square icon.
 */
 @property (nonatomic) IBInspectable BOOL isIconSquare;
 
 /**
- @return Current selected button
+ Image for radio button icon (optional).
+ */
+@property (nonatomic) IBInspectable UIImage *icon;
+
+/**
+ Image for radio button icon when selected (optional).
+ */
+@property (nonatomic) IBInspectable UIImage *iconSelected;
+
+/**
+ @return Current selected button in same group.
  */
 - (DLRadioButton *)selectedButton;
 
 /**
- Clear selection for other buttons
+ Clear selection for other buttons in in same group.
 */
 - (void)deselectOtherButtons;
 
