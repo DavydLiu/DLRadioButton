95f2fb811f6a24068248d3416334edfe04df56da
David <xingruo.liu@gmail.com>
2016-04-02 11:49:43 -0700

    Added support for RTL interface layout. Updated API comments.

--- Example/DLRadioButton.framework/Headers/DLRadioButton.h
+++ Example/DLRadioButton.framework/Headers/DLRadioButton.h
@@ -1,85 +1,89 @@
 #import <UIKit/UIKit.h>
 
 /**
- A hightly customizable Radio Button for iOS.
-*/
+ *  A hightly customizable Radio Button for iOS.
+ */
 IB_DESIGNABLE
 @interface DLRadioButton : UIButton
 
-/**@name Properties*/
 /**
- Container for holding other buttons in same group.
-*/
+ *  Container for holding other buttons in same group.
+ */
 @property (nonatomic) IBOutletCollection(DLRadioButton) NSArray *otherButtons;
 
 /**
- Size of icon, default is kDefaulIconSize.
-*/
+ *  Size of icon, default is kDefaulIconSize.
+ */
 @property (nonatomic) IBInspectable CGFloat iconSize;
 
 /**
- Color of icon, default is title color for current UIControlState.
+ *  Color of icon, default is title color for current UIControlState.
  */
 @property (nonatomic) IBInspectable UIColor *iconColor;
 
 /**
- Stroke width of icon, default is iconSize / 9.
+ *  Stroke width of icon, default is iconSize / 9.
  */
 @property (nonatomic) IBInspectable CGFloat iconStrokeWidth;
 
 /**
- Size of selection indicator, default is iconSize * 0.5.
+ *  Size of selection indicator, default is iconSize * 0.5.
  */
 @property (nonatomic) IBInspectable CGFloat indicatorSize;
 
 /**
- Color of selection indicator, default is title color for current UIControlState.
-*/
+ *  Color of selection indicator, default is title color for current UIControlState.
+ */
 @property (nonatomic) IBInspectable UIColor *indicatorColor;
 
 /**
- Margin width between icon and title, default is kDefaultMarginWidth.
+ *  Margin width between icon and title, default is kDefaultMarginWidth.
  */
 @property (nonatomic) IBInspectable CGFloat marginWidth;
 
 /**
- Whether icon on the right side, default is NO.
-*/
-@property (nonatomic, getter=isIconOnRight) IBInspectable BOOL iconOnRight;
+ *  Whether icon on the right side, default is NO.
+ *  @warning Please also set contentHorizontalAlignment to UIControlContentHorizontalAlignmentRight.
+ */
+@property (nonatomic, getter = isIconOnRight) IBInspectable BOOL iconOnRight;
 
 /**
- Whether use square icon, default is NO.
-*/
-@property (nonatomic, getter=isIconSquare) IBInspectable BOOL iconSquare;
+ *  Whether use square icon, default is NO.
+ */
+@property (nonatomic, getter = isIconSquare) IBInspectable BOOL iconSquare;
 
 /**
- Image for radio button icon (optional).
+ *  Image for radio button icon (optional).
  */
 @property (nonatomic) IBInspectable UIImage *icon;
 
 /**
- Image for radio button icon when selected (optional).
+ *  Image for radio button icon when selected (optional).
  */
 @property (nonatomic) IBInspectable UIImage *iconSelected;
 
 /**
- Whether enable multiple selection, default is NO.
+ *  Whether enable multiple selection, default is NO.
  */
-@property (nonatomic, getter=isMultipleSelectionEnabled) BOOL multipleSelectionEnabled;
+@property (nonatomic, getter = isMultipleSelectionEnabled) BOOL multipleSelectionEnabled;
 
 /**
- @return Selected button in same group.
+ *  Finds out selected button in same group.
+ *
+ *  @return Selected button.
  */
 - (DLRadioButton *)selectedButton;
 
 /**
- @return Selected buttons in same group, use it only if multiple selection is enabled.
+ *  Finds out selected buttons in same group, use it only if multiple selection is enabled.
+ *
+ *  @return Selected buttons.
  */
 - (NSArray *)selectedButtons;
 
 /**
- Clears selection for other buttons in in same group.
-*/
+ *  Clears selection for other buttons in in same group.
+ */
 - (void)deselectOtherButtons;
 
 @end
